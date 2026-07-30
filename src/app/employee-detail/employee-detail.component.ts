70a4982191fb1fe1e177ea4ee68cea8f5e77ffe7
Debashis Moharana <89587180+debashism-cybage@users.noreply.github.com>
2026-07-30 19:03:10 +0530

    AI Generated src/app/employee-detail/employee-detail.component.ts

--- src/app/employee-detail/employee-detail.component.ts
+++ src/app/employee-detail/employee-detail.component.ts
@@ -1,11 +1,10 @@
+src/app/employee-detail/employee-detail.component.ts
+
 ```typescript
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { EmployeeService } from '../employee.service';
-import { Employee } from '../employee.model';
-import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { EmployeeService } from '../../services/employee.service';
+import { Employee } from '../../models/employee.model';
 
 @Component({
   selector: 'app-employee-detail',
@@ -15,27 +14,30 @@ import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 export class EmployeeDetailComponent implements OnInit {
 
   employee: Employee | undefined;
-  searchControl = new FormControl();
-  filteredEmployees: Observable<Employee[]>;
 
-  constructor(private employeeService: EmployeeService, private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      const id = Number(params.get('id'));
-      this.employeeService.getEmployeeById(id).subscribe(employee => this.employee = employee);
-    });
-
-    this.filteredEmployees = this.searchControl.valueChanges.pipe(
-      debounceTime(300),
-      distinctUntilChanged(),
-      switchMap(value => this.employeeService.searchEmployees(value))
-    );
-  }
-
-  searchEmployees(query: string): void {
-    this.employeeService.searchEmployees(query).subscribe(employees => this.employee = employees.find(emp => emp.id === Number(this.route.snapshot.paramMap.get('id'))));
+    const employeeId = this.route.snapshot.paramMap.get('id');
+    if (employeeId) {
+      this.employeeService.getEmployeeById(+employeeId).subscribe(employee => {
+        this.employee = employee;
+      });
+    }
   }
 
 }
+```
+
+src/app/employee-detail/employee-detail.component.html
+
+```html
+<div *ngIf="employee" class="employee-detail">
+  <h2>{{ employee.name }}</h2>
+  <p><strong>Position:</strong> {{ employee.position }}</p>
+  <p><strong>Office:</strong> {{ employee.office }}</p>
+  <p><strong>Age:</strong> {{ employee.age }}</p>
+  <p><strong>Start Date:</strong> {{ employee.startDate | date }}</p>
+  <p><strong>Salary:</strong> {{ employee.salary }}</p>
+</div>
 ```
