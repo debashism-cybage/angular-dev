95dc45042c71a63d503076922b95bc6d57b15ef1
Debashis Moharana <89587180+debashism-cybage@users.noreply.github.com>
2026-08-06 19:02:17 +0530

    feat(SCRUM-9): modify src/app/home/home.ts

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -4,6 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   standalone: true,
   imports: [],
-  template: `<h1>Home</h1>`
+  template: `<div>Home</div>`
 })
 export class Home {}
