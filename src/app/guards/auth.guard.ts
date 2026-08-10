d6588f10d34aebc8cc96b1a36c0da3c2217e86f3
Debashis Moharana <89587180+debashism-cybage@users.noreply.github.com>
2026-08-10 15:58:37 +0530

    feat(SCRUM-16): modify src/app/guards/auth.guard.ts

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,22 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { filter, map, take } from 'rxjs/operators';
 
-export const authGuard: CanActivateFn = () => {
+export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (authService.isAuthenticated()) {
-    return true;
-  }
-
-  router.navigate(['/login']);
-  return false;
+  return authService.isLoading$.pipe(
+    filter(isLoading => !isLoading),
+    take(1),
+    map(() => {
+      if (authService.isAuthenticated()) {
+        return true;
+      } else {
+        router.navigate(['/login']);
+        return false;
+      }
+    })
+  );
 };
