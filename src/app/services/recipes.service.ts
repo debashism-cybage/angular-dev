d767aacc946baad47e60d4b81de48c2a111af5ef
Debashis Moharana <89587180+debashism-cybage@users.noreply.github.com>
2026-08-10 20:32:38 +0530

    feat(SCRUM-10): create src/app/services/recipes.service.ts

---
+++ src/app/services/recipes.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Recipe, RecipesResponse } from '../models/recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  private apiUrl = 'https://dummyjson.com/recipes';
+
+  constructor(private http: HttpClient) {}
+
+  getRecipes(skip: number, limit: number): Observable<RecipesResponse> {
+    const params = new HttpParams()
+      .set('skip', skip.toString())
+      .set('limit', limit.toString());
+
+    return this.http.get<RecipesResponse>(this.apiUrl, { params }).pipe(
+      catchError((error) => {
+        return throwError(() => error);
+      })
+    );
+  }
+}
