9feb74ea1000ae75f6a7653558220e1c1ecccab4
Debashis Moharana <89587180+debashism-cybage@users.noreply.github.com>
2026-08-10 21:05:38 +0530

    fix(SCRUM-14): create missing dependency src/app/exercises/exercise.model.ts

---
+++ src/app/exercises/exercise.model.ts
@@ -0,0 +1,9 @@
+export interface Exercise {
+  id: string;
+  name: string;
+  gifUrl: string;
+  bodyParts: string[];
+  targetMuscles: string[];
+  secondaryMuscles: string[];
+  equipments: string[];
+}
