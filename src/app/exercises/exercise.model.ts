d739302df9168231274ce39b811d7f40d4b42db1
Rohan Kshirsagar <83403680+rohancybage@users.noreply.github.com>
2026-08-10 20:28:21 +0530

    Revert "[Dark Factory] SCRUM-14"

--- src/app/exercises/exercise.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Exercise {
-  id: string;
-  name: string;
-  gifUrl: string;
-  bodyParts: string[];
-  targetMuscles: string[];
-  secondaryMuscles: string[];
-  equipments: string[];
-}
